3497a2853dfca3474c02825f5c525333e491a795
JanSternJohansen <jaj@tec.dk>
2023-12-04 19:02:50 +0100

    Commint mandag 4.12 aften

---
+++ TalsysConverter/TalsysConverter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalsysConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String decStr = TalsysConv.IntToDec(123);
+            Console.WriteLine("Tallet 123 skulle gerne være: " + decStr);
+
+            String binStr = TalsysConv.IntToBin(123);
+            Console.WriteLine("Tallet 123 skulle gerne være: " + binStr);
+
+            String hexStr = TalsysConv.IntToHex(123);
+            Console.WriteLine("Tallet 123 skulle gerne være: " + hexStr);
+
+
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ TalsysConverter/TalsysConverter/TalsysConv.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalsysConverter
+{
+    internal class TalsysConv
+    {
+        public static String IntToDec(int tal)
+        {
+            String decStr = "";
+
+            int temp = tal;
+
+            while(temp > 0)
+            {
+                int cif = temp % 10;
+                char cifChar = (char)(cif + 0x30);
+                decStr = cifChar + decStr;
+                temp = temp / 10;
+            }
+            return decStr;
+        }
+
+        internal static string IntToBin(int tal)
+        {
+            String binStr = "";
+
+            int temp = tal;
+
+            while (temp > 0)
+            {
+                int cif = temp % 2;
+                char cifChar = (char)(cif + 0x30);
+                binStr = cifChar + binStr;
+                temp = temp / 2;
+            }
+            return binStr;
+        }
+
+        internal static string IntToHex(int tal)
+        {
+            String hexStr = "";
+
+            int temp = tal;
+
+            char cifChar = (char)0;
+
+            while (temp > 0)
+            {
+                int cif = temp % 16;
+                if(cif < 10)
+                {
+                    cifChar = (char)(cif + 0x30);
+                }
+                if (cif >= 10)
+                {
+                    cifChar = (char)(cif + 0x31);
+                }
+                
+                hexStr = cifChar + hexStr;
+                temp = temp / 16;
+            }
+            return hexStr;
+
+        }
+    }
+
+
+}
