8eb4d84f8b560495ad68c970b90b36153678a3b6
JanSternJohansen <jaj@tec.dk>
2023-12-04 10:38:26 +0100

    Commit mandag 4.dec kl 10:38

---
+++ ASCIITest/ASCIITest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCIITest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 256; i++)
+            {
+                Console.Write("{0,3} {0,3:X} {1}  ", i, (char)i);
+
+                if (i % 10 == 9)
+                {
+                    Console.WriteLine();
+                }
+            }
+
+            String strCifre = "0123456789ABCDEFabcdef";
+
+            foreach(char ch in strCifre)
+            {
+                Console.WriteLine("{0,3} {0,3:X} {1} ", (int)ch, ch);
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
